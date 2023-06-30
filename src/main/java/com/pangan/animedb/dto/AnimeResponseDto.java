5624890190d8367de49e01c32240b842218640a4
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-06-30 11:32:20 +0800

    Initial commit

---
+++ src/main/java/com/pangan/animedb/dto/AnimeResponseDto.java
@@ -0,0 +1,7 @@
+package com.pangan.animedb.dto;
+
+public record AnimeResponseDto(
+        String id,
+        String name
+) {
+}
---
+++ src/test/java/com/pangan/animedb/AnimedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangan.animedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
