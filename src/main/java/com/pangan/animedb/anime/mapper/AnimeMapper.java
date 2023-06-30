ef57d551c3068d44f2500399cb6c51403f8e3b30
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-06-30 15:19:06 +0800

    Adding new anime in AnimeMapper.java and AnimeService.java

---
+++ src/main/java/com/pangan/animedb/anime/mapper/AnimeMapper.java
@@ -0,0 +1,21 @@
+package com.pangan.animedb.anime.mapper;
+
+import com.pangan.animedb.anime.dto.AnimeRequestDto;
+import com.pangan.animedb.anime.dto.AnimeResponseDto;
+import com.pangan.animedb.anime.model.Anime;
+
+public class AnimeMapper {
+
+    public static Anime mapRequestToAnime(AnimeRequestDto animeRequestDto) {
+        Anime anime = new Anime();
+        anime.setName(animeRequestDto.name());
+        return anime;
+    }
+
+    public static AnimeResponseDto mapAnimeToResponse(Anime anime) {
+        return new AnimeResponseDto(
+                anime.getId(),
+                anime.getName()
+        );
+    }
+}
--- src/main/java/com/pangan/animedb/anime/service/AnimeService.java
+++ src/main/java/com/pangan/animedb/anime/service/AnimeService.java
@@ -1,9 +1,10 @@
 package com.pangan.animedb.anime.service;
 
 import com.pangan.animedb.anime.dto.AnimeRequestDto;
+import com.pangan.animedb.anime.dto.AnimeResponseDto;
 import com.pangan.animedb.anime.model.Anime;
 import com.pangan.animedb.anime.repository.AnimeRepository;
-import com.pangan.animedb.mapper.AnimeMapper;
+import com.pangan.animedb.anime.mapper.AnimeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +18,9 @@ public class AnimeService {
         this.animeRepository = animeRepository;
     }
 
-    // TODO: Test if this is working.
-    // - Make the String into Optional<AnimeResponseDto>
-    // - Make the mapper into Optional<Anime>
-    public String addAnime(AnimeRequestDto animeRequestDto) {
-        Anime anime = AnimeMapper.mapAnimeRequestDtoToAnime(animeRequestDto);
-        animeRepository.save(anime);
-        return "Add an anime";
+    public AnimeResponseDto addAnime(AnimeRequestDto animeRequestDto) {
+        Anime newAnime = AnimeMapper.mapRequestToAnime(animeRequestDto);
+        Anime savedAnime = animeRepository.save(newAnime);
+        return AnimeMapper.mapAnimeToResponse(savedAnime);
     }
 }
