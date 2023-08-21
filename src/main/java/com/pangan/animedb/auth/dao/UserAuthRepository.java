6f8fb100c43fa129a85fb4e2743071e6b87d2e9f
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-08-21 10:37:59 +0800

    Commented Spring Security

--- src/main/java/com/pangan/animedb/auth/dao/UserAuthRepository.java
+++ src/main/java/com/pangan/animedb/auth/dao/UserAuthRepository.java
@@ -1,12 +1,12 @@
-package com.pangan.animedb.auth.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface UserAuthRepository extends MongoRepository<UserAuth, String> {
-    Optional<UserAuth> findByUsername(String username);
-    boolean existsByUsername(String username);
-}
+//package com.pangan.animedb.auth.dao;
+//
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface UserAuthRepository extends MongoRepository<UserAuth, String> {
+//    Optional<UserAuth> findByUsername(String username);
+//    boolean existsByUsername(String username);
+//}
